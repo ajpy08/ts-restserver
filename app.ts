3f7c24bfd018e28e08e11c11969d97d80d0f00d3
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-02-16 13:34:38 -0600

    Termino configuracion de TypeScript y TSLint

---
+++ app.ts
@@ -0,0 +1,3 @@
+export const nombre = 'javi';
+
+console.log(nombre);
---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = void 0;
+exports.nombre = 'javi';
+console.log(exports.nombre);
+//# sourceMappingURL=app.js.map
